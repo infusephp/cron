89b70c7d7a324c9d4bc0d0876ac4fbacf8c1445d
Jared King <j@jaredtking.com>
2016-06-16 11:38:47 -0500

    bugfix: make DI available to console command

--- src/Console/RunScheduledCommand.php
+++ src/Console/RunScheduledCommand.php
@@ -11,12 +11,15 @@
 namespace App\Cron\Console;
 
 use App\Cron\Libs\JobSchedule;
+use Infuse\HasApp;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class RunScheduledCommand extends Command
 {
+    use HasApp;
+
     protected function configure()
     {
         $this
