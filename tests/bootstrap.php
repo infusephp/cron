98ad17c24ae315171e2dbd6fd9982ece794e62d2
Jared King <j@jaredtking.com>
2017-06-18 12:54:04 -0500

    fix phpunit configuration

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @author Jared King <j@jaredtking.com>
+ *
+ * @link http://jaredtking.com
+ *
+ * @copyright 2015 Jared King
+ * @license MIT
+ */
+require_once __DIR__.'/../vendor/autoload.php';
